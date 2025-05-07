b504fb257507505fb4df49b9f3f225fb63793a55
Shiri2005 <shirishasha925@gmail.com>
2025-05-07 23:03:45 +0530

    Add files via upload
    dice challenge

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-function genQ(){
-
-    fetch('https://dummyjson.com/quotes').then(response => response.json()).then(data => {
-
-        let random = Math.floor(Math.random() * 30);
-    
-        let uniqueQ = data.quotes[ random ].quote;
-    
-        let para = document.getElementsByTagName('p');
-    
-        para[0].innerText = uniqueQ;
-        
-    })
-}
+console.log("hello")
