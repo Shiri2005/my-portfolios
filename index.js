4b3ce257a92848733b7241705492ed8b141b4b78
Shiri2005 <shirishasha925@gmail.com>
2025-07-04 18:12:13 +0530

    Add files via upload
    secrete project

--- index.js
+++ index.js
@@ -1,48 +1,42 @@
-var numberOfDrumButtons=document.querySelectorAll(".drum").length;
-for(var i=0;i<numberOfDrumButtons;i++){
-document.querySelectorAll(".drum")[i].addEventListener("click",function(){
-    var buttonInnerHTML=this.innerHTML;
-    switch(buttonInnerHTML){
-        case "w":
-        var kick=new Audio("sounds/kick-bass.mp3");
-        kick.play();
-        break;
-        case "a":
-        var snare=new Audio("sounds/snare.mp3");
-        snare.play();
-        break;
-        case "s":
-        var tom1=new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-        case "d":
-        var tom2=new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;case "j":
-        var tom3=new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break;
-        case "k":
-        var tom4=new Audio("sounds/tom-4.mp3");
-        tom4.play();
-        break;
-        case "l":
-        var crash=new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+import express from "express";
+import bodyParser from "body-parser";
+import {dirname} from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express()
+const port=3000;
+
+var userIsAuthorised = false;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function passwordCheck(req,res,next){
+    const password =req.body["password"];
+    if (password==="IloveProgamming"){
+        userIsAuthorised=true;
+
     }
+    next();
 
-});
 }
+app.use(passwordCheck);
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/public/index.html");
+});
+app.post("/check",(req,res)=>{
+    if (userIsAuthorised) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.sendFile(__dirname + "/public/index.html");
+    //Alternatively res.redirect("/");
+  }
+});
 
-
-
-
-
-
-
-
-
-
-
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});  
 
