bf146609406400a860a16ef8d663615cbf2e32d4
Shiri2005 <shirishasha925@gmail.com>
2025-07-05 22:43:46 +0530

    Add files via upload
    greetings in the web browser

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-//To see how the final website should work, run "node solution.js".
-//Make sure you have installed all the dependencies with "npm i".
-//The password is ILoveProgramming
 import express from "express";
 import bodyParser from "body-parser";
 import {dirname} from "path";
@@ -10,33 +7,24 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express()
 const port=3000;
 
-var userIsAuthorised = false;
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-function passwordCheck(req,res,next){
-    const password =req.body["password"];
-    if (password==="IloveProgamming"){
-        userIsAuthorised=true;
 
-    }
-    next();
-
-}
-app.use(passwordCheck);
 app.get("/",(req,res)=>{
-    res.sendFile(__dirname+"/public/index.html");
+   const d = new Date();
+   let day = d.getDay();
+   if (day == 0 || day == 6) {
+     res.send("Its a weekend lets chill")
+   }else{
+    res.send("its a weekday lets work hard with a happy smile");
+
+   }
+     res.send(`<h1>${message}</h1>`);
 });
-app.post("/check",(req,res)=>{
-    if (userIsAuthorised) {
-    res.sendFile(__dirname + "/public/secret.html");
-  } else {
-    res.sendFile(__dirname + "/public/index.html");
-    //Alternatively res.redirect("/");
-  }
-});
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });  
-
