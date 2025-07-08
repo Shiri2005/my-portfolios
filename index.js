d15b0c653174dada18d0eb7730e7cf1c2adca4b1
Shiri2005 <shirishasha925@gmail.com>
2025-07-08 12:52:42 +0530

    Add files via upload
    A dynamic web page developed  with partials

--- index.js
+++ index.js
@@ -1,37 +1,17 @@
 import express from "express";
-import bodyParser from "body-parser";
-import {dirname} from "path";
-import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
 
-const app = express()
-const port=4000;
-
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
-app.get("/",(req,res)=>{
-   const d = new Date();
-   let day = d.getDay();
-   const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const dayName = dayNames[day];
-
-let message = `Happy ${dayName}!`;
-   if (day == 0 || day == 6) {
-     message += "It's a weekend- lets chill"  
-   }else{
-    message += "its a weekday lets work hard with a happy smile";
-
-   }
-
-     res.render("index",{message});
+app.get("/", (req, res) => {
+  const data = {
+    title: "EJS Tags",
+    seconds: new Date().getSeconds(),
+    items: ["apple", "banana", "cherry"],
+    htmlContent: "<strong>This is some strong text</strong>",
+  };
+  res.render("index.ejs", data);
 });
 
-
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});  
+  console.log(`Server is running on port ${port}`);
+});
