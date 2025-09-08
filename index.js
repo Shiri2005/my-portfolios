6cbc9e4384ede70d7ec7e506758c49f86cfe2651
Shirisha.Y <shirishasha925@gmail.com>
2025-09-08 22:41:33 +0530

    destructing array and objects
    destructing array and objects

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
+//CHALLENGE: uncomment the code below and see the car stats rendered
 import React from "react";
 import ReactDOM from "react-dom";
-import "./../public/styles.css";
-import App from "./components/App";
+import cars from "./practice";
+const [honda, tesla] = cars;
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const {
+  speedStats: { topSpeed: hondaTopSpeed },
+} = honda;
+const {
+  speedStats: { topSpeed: teslaTopSpeed },
+} = tesla;
+
+const {
+  coloursByPopularity: [hondaTopColour],
+} = honda;
+const {
+  coloursByPopularity: [teslaTopColour],
+} = tesla;
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+      <th>Top Color</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
